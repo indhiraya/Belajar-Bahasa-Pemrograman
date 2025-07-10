82a6d5e66a574394a2e95105f21b5e5b5798b079
Indhira Ayu Puspita Ningrum <162036447+indhiraya@users.noreply.github.com>
2025-07-10 09:11:08 +0700

    Update perkenalan_rekursi.cpp

--- c++/toki/Pemrograman Dasar dengan C++/09. Rekursi/perkenalan_rekursi.cpp
+++ c++/toki/Pemrograman Dasar dengan C++/09. Rekursi/perkenalan_rekursi.cpp
@@ -16,3 +16,18 @@ int faktorial(int n) {
 int main() {
     cout << faktorial(12) << endl;
 }
+//u can use this too
+#include <iostream>
+using namespace std;
+
+int faktorial(int n) {
+    if(n==1 || n==0){
+        return 1;
+    }
+    
+    return n*faktorial(n-1);
+}
+
+int main() {
+    cout << faktorial(12) << endl;
+}
